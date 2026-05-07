54a6a1d8de6d4d1cc01200fc7b8a054182991b7b
vvlaads <vvlaads@gmail.com>
2026-05-07 18:52:24 +0300

    Added logger, fixed closing drivers

--- src/test/java/com/swt/lab/SeleniumTest.java
+++ src/test/java/com/swt/lab/SeleniumTest.java
@@ -1,11 +1,14 @@
 package com.swt.lab;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -14,7 +17,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SeleniumTest {
+
     public List<WebDriver> driverList;
+    private static final Logger log = LoggerFactory.getLogger(SeleniumTest.class);
 
     @BeforeEach
     public void setUp() {
@@ -28,8 +33,21 @@ public class SeleniumTest {
         if (config.getBoolean("use.firefox")) {
             driverList.add(new FirefoxDriver());
         }
+
+        log.info("Drivers initialized: {}", driverList.size());
     }
 
+    @AfterEach
+    public void tearDown() {
+        driverList.forEach(driver -> {
+            try {
+                log.info("Closing driver");
+                driver.quit();
+            } catch (Exception e) {
+                log.error("Error while closing driver", e);
+            }
+        });
+    }
 
     private void switchToNewTab(WebDriver driver) {
         String originalWindow = driver.getWindowHandle();
@@ -40,43 +58,56 @@ public class SeleniumTest {
         for (String windowHandle : driver.getWindowHandles()) {
             if (!windowHandle.equals(originalWindow)) {
                 driver.switchTo().window(windowHandle);
+                log.info("Switched to new tab");
                 return;
             }
         }
+
+        throw new RuntimeException("New tab was not found");
     }
 
     @Test
     public void homeTest() {
-        driverList.forEach(webDriver -> {
-            HomePage homePage = new HomePage(webDriver);
+        driverList.forEach(driver -> {
+
+            log.info("START HOME TEST");
+
+            HomePage homePage = new HomePage(driver);
+
             homePage.open();
+            homePage.waitUntilLoaded();
 
             assertTrue(homePage.isOpened());
-            webDriver.quit();
+
+            log.info("END HOME TEST");
         });
     }
 
     @Test
     public void loginTest() {
-        driverList.forEach(webDriver -> {
-            HomePage homePage = new HomePage(webDriver);
+        driverList.forEach(driver -> {
+
+            log.info("START LOGIN TEST");
+
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = new LoginPage(driver);
+            ProfilePage profilePage = new ProfilePage(driver);
+
             homePage.open();
             homePage.waitUntilLoaded();
             homePage.clickLogin();
 
-            switchToNewTab(webDriver);
+            switchToNewTab(driver);
 
             String login = EnvReader.get("LOGIN");
             String password = EnvReader.get("PASSWORD");
 
-            LoginPage loginPage = new LoginPage(webDriver);
             loginPage.waitUntilLoaded();
             loginPage.login(login, password);
 
-            ProfilePage profilePage = new ProfilePage(webDriver);
             assertTrue(profilePage.isOpened());
 
-            webDriver.quit();
+            log.info("END LOGIN TEST");
         });
     }
 }
